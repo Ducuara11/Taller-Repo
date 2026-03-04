702ed3849858ae98f1343db844addc4f84d762b5
Ducuara11 <ducuara19@hotmail.com>
2026-03-03 21:58:28 -0500

    ejercicio 1 - primera API con FastAPI y ruta saludo

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hola mundo")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "Bienvenido a mi primera API"}
+
+@app.get("/saludo/{nombre}")
+def saludo(nombre: str):
+    return {"mensaje": f"Hola {nombre}, bienvenido!"}
